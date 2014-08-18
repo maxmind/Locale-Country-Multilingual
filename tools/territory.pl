0589463739ab932dee31ee932deb4ada30a5d608
Gregory Oschwald <goschwald@maxmind.com>
2014-08-18 10:32:18 -0700

    Updated CLDR data (June 2013)

--- tools/territory.pl
+++ tools/territory.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use autodie ':all';
 
 use Getopt::Std;
 use Pod::Usage;
@@ -18,65 +19,76 @@ $parser->keep_blanks(0);
 
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 
-getopts('hi:v', \my %opts);
+getopts( 'hi:v', \my %opts );
 pod2usage() if $opts{h};
-if ($opts{v}) {
+if ( $opts{v} ) {
     $VERSION =~ /(\d\S+)/;
     die "$0 $1\n";
 }
 
-my %Countries;
-my $iso_file = $opts{i};
-my %IsoData;
-my ($a2, $a3, $n3, $name);
+for my $file (@ARGV) {
+    process_language($file);
+}
 
-if ($iso_file) {
-    my $csv = Text::CSV_XS->new ({binary => 1, sep_char => ';'});
-    open my $fh, '<:utf8', $iso_file
-	or die "Unable to open iso index file: $!\n";
-    <$fh>;	# skip first line
-    while (my $row = $csv->getline($fh)) {
-	$IsoData{uc($row->[0])} = [uc($row->[1]), $row->[2], $row->[3]];
+sub process_language {
+    my $file = shift;
+
+    my ($language) = $file =~ m{/([^/]+).xml};
+
+    my $iso_file = $opts{i};
+    my %iso_data;
+    my ( $a2, $a3, $n3, $name );
+
+    if ($iso_file) {
+        my $csv = Text::CSV_XS->new( { binary => 1, sep_char => ';' } );
+        open my $fh, '<:encoding(UTF-8)', $iso_file;
+        <$fh>;    # skip first line
+        while ( my $row = $csv->getline($fh) ) {
+            $iso_data{ uc( $row->[0] ) }
+                = [ uc( $row->[1] ), $row->[2], $row->[3] ];
+        }
+        close $fh;
     }
-    close $fh;
-}
 
-if (scalar @ARGV) {
-    foreach (@ARGV) {
-        handle_territories($parser->parse_file($_));
+
+    my %countries = handle_territories( $parser->parse_file($file) );
+
+    open my $out_fh, '>:encoding(UTF-8)', "lib/Locale/Country/Multilingual/$language.dat";
+    for my $a2 ( sort keys %iso_data ) {
+        ( $a3, $n3, $name ) = @{ $iso_data{$a2} };
+        print {$out_fh} join(
+            ':', $a2, $a3, $n3,
+            ( ref( $countries{$a2} ) eq 'ARRAY' )
+            ? @{ $countries{$a2} }
+            : $name
+            ),
+            "\n";
     }
-}
-else {
-    # read from stdin
-    my @doc = <STDIN>;
-    my $string = join "", @doc;
-    handle_territories($parser->parse_string($string));
-}
-
-foreach $a2 (sort keys %IsoData) {
-    ($a3, $n3, $name) = @{$IsoData{$a2}};
-    print join(':', $a2, $a3, $n3, (ref($Countries{$a2}) eq 'ARRAY') ? @{$Countries{$a2}} : $name), "\n";
+    close $out_fh;
 }
 
 sub handle_territories {
-    my ($territories) = $_[0]->findnodes('//territories');
-    my (@attr, $id, $name);
+    my ($territories) = shift->findnodes('//territories');
+    my ( @attr, $id, $name );
 
     die "No territories in file\n"
-	unless blessed $territories and $territories->isa('XML::LibXML::Element');
+        unless blessed $territories
+        and $territories->isa('XML::LibXML::Element');
 
-    foreach my $territory ($territories->childNodes) {
-	next
-	    unless blessed $territory and
-		$territory->isa('XML::LibXML::Element');
-	@attr = $territory->attributes;
-	$id = $territory->getAttributeNode('type');
-	next
-	    unless blessed $id and $id->value =~ /^[A-Z]{2}$/;
-	
-	($name = $territory->to_literal) =~ s/:/ /g;	# : not allowed
-	push @{$Countries{uc($id->value)}}, $name;
+    my %countries;
+    foreach my $territory ( $territories->childNodes ) {
+        next
+            unless blessed $territory
+            and $territory->isa('XML::LibXML::Element');
+        @attr = $territory->attributes;
+        $id   = $territory->getAttributeNode('type');
+        next
+            unless blessed $id and $id->value =~ /^[A-Z]{2}$/;
+
+        ( $name = $territory->to_literal ) =~ s/:/ /g;    # : not allowed
+        push @{ $countries{ uc( $id->value ) } }, $name;
     }
+    return %countries;
 }
 
 __END__
